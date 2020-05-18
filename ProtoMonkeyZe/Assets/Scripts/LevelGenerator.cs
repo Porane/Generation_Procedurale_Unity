49df0a3360a8cd2997c16ae562f9ed92ea5e1e70
Porane <alexis.wallisch@gmail.com>
2020-05-18 20:13:01 +0200

    Prototype fonctionnel

---
+++ ProtoMonkeyZe/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private const float PLAYERDISTANCESPAWN = 30f;
+
+    [SerializeField] private Transform levelPartStart;
+    [SerializeField] private List<Transform> levelPartList;
+    [SerializeField] private Player player;
+
+    private Vector2 lastEndPosition;
+    
+    // Start is called before the first frame update    
+    void Start()
+    {
+        lastEndPosition = levelPartStart.Find("EndPosition").position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(player.GetPosition(), lastEndPosition) < PLAYERDISTANCESPAWN)
+        {
+            SpawnLevelParts();
+        }
+    }
+
+    private void SpawnLevelParts()
+    {
+        Transform chosenLevelPart = levelPartList[Random.Range(0, levelPartList.Count)];
+        Transform lastLevelPartTransform = SpawnLevelParts(chosenLevelPart, lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevelParts(Transform levelPart, Vector2 spawnPosition)
+    {
+        Transform levelPartsTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
+        return levelPartsTransform;
+    }
+}
---
+++ ProtoMonkeyZe/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+    public bool running = false;
+    public float moveSpeed = 10f;
+    public float jumpVelocity = 35f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && running == false)
+        {
+            running = true;
+        }
+
+        if (running == true)
+        {
+            rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+        }
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+
+    }
+
+    public Vector2 GetPosition()
+    {
+        return transform.position;
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycasthit2d =  Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, layerMask);
+        return raycasthit2d.collider != null;
+    }
+}
